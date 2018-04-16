070178c6bf3885a3451b79613a5866f7551ffa63
mickdineen <x16350523@student.ncirl.ie>
2018-04-16 15:10:41 +0100

    Add files via upload

---
+++ ChangeMusicVolume.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ChangeMusicVolume : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Slider Volume;
+	public AudioSource MyMusic;
+
+
+	// Update is called once per frame
+
+	void update () {
+	
+		MyMusic.volume = Volume.value;
+	
+	}
+
+
+}
---
+++ PauseMenu.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+public class PauseMenu : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+	public GameObject PauseMenuUI;
+
+
+	void Start() {
+		PauseMenuUI.SetActive (false);
+	}
+
+	void Update(){
+		if (Input.GetButtonDown ("Pause")) {
+			GameIsPaused = !GameIsPaused;
+		}
+		if(GameIsPaused){
+			PauseMenuUI.SetActive (true);
+			Time.timeScale = 0;
+			
+	}
+		if (!GameIsPaused) {
+		
+			PauseMenuUI.SetActive (false);
+			Time.timeScale = 1;
+		}
+			
+	
+	//void Update () {
+
+	//	if(Input.GetKeyDown(KeyCode.Escape))
+	
+
+	//	if(GameIsPaused){
+	//	Resume();
+		} 
+	//else
+	//	{
+
+	//	Pause();
+	//	}
+	//}
+
+
+		public void Resume()
+	{
+		PauseMenuUI.SetActive(false);
+		Time.timeScale= 1f;
+		GameIsPaused = false;
+	}
+
+		void Pause()
+	{
+		PauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+	public void LoadMenu()
+	{
+		EditorSceneManager.LoadScene("April8");
+	}
+	public void DeMenu ()
+	{
+		Debug.Log ("Quitting game ...");
+		Application.Quit();
+	}
+		}
